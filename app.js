b6e82c5044fe4de4816863e3c3e6f0d492829fa5
keyanbagheri <keyanbagheri@gmail.com>
2015-02-21 10:22:50 -0800

    adding basic ajax search functionality

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ var MovieListView = Backbone.View.extend({
   },
   render: function(){
     var self = this;
-    // empty our object to start anew
+    // empty our view to start anew
     self.$el.empty();
     _.each(self.collection.models, function(movie){
       var newMovieView = new MovieView({ model: movie});
@@ -37,8 +37,8 @@ var MovieListView = Backbone.View.extend({
 $( document ).ready(function() {
   var newMovieCollection = new MovieCollection();
 
-  var newMovie = new Movie({ title: "Untitled Adam Sandler Crap", year: "2015"});
-  var newerMovie = new Movie({ title: "Michael Moore Talks Crap", year: "2015 1/2"});
+  var newMovie = new Movie({ title: "Untitled Adam Sandler Crap"});
+  var newerMovie = new Movie({ title: "Michael Moore Talks Crap"});
 
   newMovieCollection.add(newMovie);
   newMovieCollection.add(newerMovie);
@@ -46,6 +46,30 @@ $( document ).ready(function() {
   var newMovieCollectionView = new MovieListView({collection: newMovieCollection, el: $('div.movie-list')});
 
   newMovieCollectionView.render();
+
+  $('form#addMovie').submit(function(e){
+    // clear our previous search collection
+    newMovieCollection.reset();
+    var movieTitle = $('form#addMovie').find('input[name="title"]').val();
+
+    // ajax search results
+    $.ajax({
+    url:      "http://www.omdbapi.com/?s=" + movieTitle,
+    dataType: 'json',
+    method:   'get'
+    }).done(function(data){
+      console.log(data)
+      _.each(data["Search"], function(movie){
+        var newestMovie = new Movie({
+          title: movie["Title"]});
+        console.log(movie);
+        newMovieCollection.add(newestMovie);
+        });
+      });
+
+    e.preventDefault();
+    return false;
+  });
 });
 
 
