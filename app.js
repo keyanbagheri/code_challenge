380074a77fd931a89bae3464c65659e3cdf939bd
keyanbagheri <keyanbagheri@gmail.com>
2015-02-21 08:49:30 -0800

    setting up basic template

--- app.js
+++ app.js
@@ -2,7 +2,14 @@
 var Movie = Backbone.Model.extend({});
 
 // create a way to display each movie
-var MovieView = Backbone.View.extend({});
+var MovieView = Backbone.View.extend({
+  tagName: 'li',
+  template: _.template($('#movie_view').html()),
+  render: function(){
+    this.$el.html(this.template(this.model.toJSON()));
+    return this;
+  }
+});
 
 // now a place to store our movies
 var MovieCollection = Backbone.Collection.extend({
@@ -11,3 +18,15 @@ var MovieCollection = Backbone.Collection.extend({
 
 // and a way to view our collection of movies
 var MovieListView = Backbone.View.extend({});
+
+
+
+
+
+
+
+
+
+
+
+
